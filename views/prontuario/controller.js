e86f03f65e7fc3df04e59aff04d335bb76766caf
Marcos Demetrio Nascimento <demetrius.marcos@gmail.com>
2016-10-29 15:05:20 -0200

    Ajuste

--- views/prontuario/controller.js
+++ views/prontuario/controller.js
@@ -14,6 +14,18 @@
 		});
 		//--
 
+		//-- Controle Panels
+		$scope.panel = [];
+		
+		$scope.collapsePanel = function(panelNum){
+			$scope.panel[panelNum].collapsed = !$scope.panel[panelNum].collapsed;
+		};
+		$scope.isPanelCollapsed = function(panelNum){
+			return $scope.panel[panelNum].collapsed;
+		};
+		//--
+
+		//--
 		$scope.listaVazia = true;
 		$scope.atendimentos = [];
 
@@ -21,6 +33,13 @@
 		$scope.OnChangeCidadao = function(cidadao) {
 			AtendimentoService.GetAll().then(function(data){
 				$scope.atendimentos = data;
+				console.log(data);
+				
+				for (var i = data.length - 1; i >= 0; i--) {
+					$scope.panel.push({
+						collapsed: false	
+					});
+				};
 
 				$scope.listaVazia = $scope.atendimentos.length === 0;
 			});
