2fa552391c1c2896b1040e9adb88b05ffd7dffd5
Marcos Demetrio Nascimento <demetrius.marcos@gmail.com>
2016-10-29 13:52:14 -0200

    PRontuario

---
+++ views/prontuario/controller.js
@@ -0,0 +1,30 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('ProntuarioController', ProntuarioController);
+
+	ProntuarioController.$inject = ['$scope', 'AtendimentoService', 'CidadaoService'];
+
+	function ProntuarioController($scope, AtendimentoService, CidadaoService) {
+		//-- Carregar lista de Cidadão
+		CidadaoService.GetAll().then(function(data){
+			$scope.cidadaos = data;
+		});
+		//--
+
+		$scope.listaVazia = true;
+		$scope.atendimentos = [];
+
+		//-- Ao selecionar um cidadão
+		$scope.OnChangeCidadao = function(cidadao) {
+			AtendimentoService.GetAll().then(function(data){
+				$scope.atendimentos = data;
+
+				$scope.listaVazia = $scope.atendimentos.length === 0;
+			});
+		}
+		//--
+	}
+})();
